380be1091bca0d6c9423d7a8dd7cf19f846c2e52
Simen A. W. Olsen <so@bjerk.io>
2021-09-01 09:37:44 +0200

    feat: first commit

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+import { exec as callbackExec } from 'child_process';
+import { promisify } from 'util';
+import cliSelect from 'cli-select';
+
+const exec = promisify(callbackExec);
+
+const main = async () => {
+  const configsRaw = await exec(
+    'gcloud config configurations list --format=json',
+  );
+  const configs = JSON.parse(configsRaw.stdout);
+  const options = configs.map((config: any) => config.name);
+  const selected = await cliSelect({
+    values: options,
+    // defaultValue: configs.find((c: any) => c.is_active).name,
+  });
+  if (selected) {
+    await exec(`gcloud config configurations activate ${selected.value}`);
+  }
+};
+
+main();
